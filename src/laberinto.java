316393671000b7e73e9bff4b59fd5d14b25c69ba
Artur0estrada <a220214323@unison.mx>
2021-11-29 18:24:37 -0700

    corridos alterados

--- src/laberinto.java
+++ src/laberinto.java
@@ -3,6 +3,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class laberinto {
     public static void main(String[] args) {
@@ -13,9 +25,39 @@ public class laberinto {
         System.out.println("Puros corridos tumbados");
     }
 
+    public static char[][] visit(char[][] map , Point start){
+        char[][] resultado = map;
+        int []x = {0,0,1,-1};
+        int []y = {1,-1,0,0};
+        LinkedList<Point> q = new LinkedList();
+        q.add(start);
+        int n = map.length;
+        int m = map[0].length;
+        int[][]dist = new int[n][m];
+        for(int []a : dist){
+            Arrays.fill(a,-1);
+        }
+        dist[start.x][start.y] = 0;
+        while(!q.isEmpty()){
+            Point p = q.removeFirst();
+            for(int i = 0; i < 4; i++){
+                int a = p.x + x[i];
+                int b = p.y + y[i];
+                if(a >= 0 && b >= 0 && a < n && b < m && dist[a][b] == -1 && map[a][b] != '+' && map[a][b] != '-' && map[a][b] != '|' ){
+                    dist[a][b] = 1 + dist[p.x][p.y];
+                    resultado[a][b]='#';
+                    if(map[a][b]==map[19][20]){
+                        break;
+                    }
+                    q.add(new Point(a,b));
+                }
+            }
+        }
+        return resultado;
+    }
+
     //Para leer el laberinto
-    public static char[][] cargaLaberinto(String archivo)
-    {
+    public static char[][] cargaLaberinto(String archivo) {
         int i=0;
         ArrayList<String> lineas = new ArrayList<>();
         try {
