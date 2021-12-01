c734862bac826e3434f5abf98fd01548d2b52a30
Artur0estrada <a220214323@unison.mx>
2021-12-01 09:44:57 -0700

    Actualizacion con DFS Y BFS

--- src/laberinto.java
+++ src/laberinto.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 class Point{
     int x;
@@ -24,16 +19,38 @@ public class laberinto {
         System.out.println("quiubole");
         System.out.println("Puros corridos tumbados");
         char[][] laberinto = cargaLaberinto(args[0]);
+        char[][] laberinto2 = cargaLaberinto(args[0]);
         for(char[] fila: laberinto){
             for(char elemento: fila){
                 System.out.print(elemento);
             }
             System.out.println();
         }
-        Point start = new Point(0,1);
-        char[][] resuelto = visit(laberinto, start);
-        System.out.println("a ver: ");
-        for(char[] fila: resuelto){
+        ArrayList<String> recorrido = new ArrayList<>();
+        ArrayList<String> recorrido2 = new ArrayList<>();
+        int startfila = 0;
+        int startcolumna = 0;
+        for (int i = 0; i < laberinto.length; i++) {
+            for (int j = 0; j < laberinto[0].length; j++) {
+                if(laberinto[i][startcolumna] == ' '){
+                    startfila = i;
+                }
+            }
+
+        }
+        Point start = new Point(startcolumna,startfila);
+        char[][] resueltoBFS = visitBFS(laberinto, start, recorrido);
+        char[][] resueltoDFS = visitDFS(laberinto2, start, recorrido2);
+        //char[][] resueltoDIJ = visitDijkstra(laberinto, start, recorrido);
+        System.out.println("BFS: ");
+        for(char[] fila: resueltoBFS){
+            for(char elemento: fila){
+                System.out.print(elemento);
+            }
+            System.out.println();
+        }
+        System.out.println("DFS: ");
+        for(char[] fila: resueltoDFS){
             for(char elemento: fila){
                 System.out.print(elemento);
             }
@@ -41,35 +58,128 @@ public class laberinto {
         }
     }
 
-    public static char[][] visit(char[][] map , Point start){
+    public static char[][] visitDFS(char[][] map , Point start, ArrayList<String> recorrido){
         char[][] resultado = map;
         int []x = {0,0,1,-1};
         int []y = {1,-1,0,0};
-        LinkedList<Point> q = new LinkedList();
-        q.add(start);
+        Stack<Point> stack = new Stack<>();
+        stack.push(start);
         int n = map.length;
         int m = map[0].length;
-        int[][]dist = new int[n][m];
-        for(int []a : dist){
-            Arrays.fill(a,-1);
-        }
-        dist[start.x][start.y] = 0;
-        while(!q.isEmpty()){
-            Point p = q.removeFirst();
-            for(int i = 0; i < 4; i++){
-                int a = p.x + x[i];
-                int b = p.y + y[i];
-                if(a >= 0 && b >= 0 && a < n && b < m && dist[a][b] == -1 && map[a][b] != '+' && map[a][b] != '-' && map[a][b] != '|' ){
-                    dist[a][b] = 1 + dist[p.x][p.y];
-                    resultado[a][b]='#';
-                    if(map[a][b]==map[19][20]){
-                        break;
-                    }
-                    q.add(new Point(a,b));
+        boolean[][] visto = new boolean[n][m];
+        int endx = resultado[0].length-1;
+        int endy = 0;
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[0].length; j++) {
+                if(resultado[i][endx] == ' '){
+                    endy = i;
                 }
             }
         }
+        boolean bandera = false;
+        while(!stack.isEmpty()){
+            if(bandera == true){
+                break;
+            }
+            Point p = stack.pop();
+            for(int i = 0; i < 4; i++){
+                int a = p.x + x[i];
+                int b = p.y + y[i];
+                if(a >= 0 && b >= 0 && a < n && b < m && visto[a][b] == false  && map[a][b] != '+' && map[a][b] != '-' && map[a][b] != '|' ){
+                    visto[a][b] = true;
+                    recorrido.add("\"(" + a + ","+b + ")\"");
+                    resultado[a][b]='#';
+                    if(map[a][b]==map[endy][endx]){
+                        bandera = true;
+                        break;
+                    }
+                    stack.push(new Point(a,b));
+                }
+            }
+        }
+        for(String recorrer: recorrido){
+            System.out.println(recorrer);
+        }
+        escribeArchivo(recorrido,resultado);
         return resultado;
+    }
+
+    public static char[][] visitBFS(char[][] map , Point start, ArrayList<String> recorrido){
+        char[][] resultado = map;
+        int []x = {0,0,1,-1};
+        int []y = {1,-1,0,0};
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(start);
+        int n = map.length;
+        int m = map[0].length;
+        boolean[][] visto = new boolean[n][m];
+        int endx = resultado[0].length-1;
+        int endy = 0;
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[0].length; j++) {
+                if(resultado[i][endx] == ' '){
+                    endy = i;
+                }
+            }
+
+        }
+        visto[start.x][start.y] = true;
+        boolean bandera = false;
+        while(!queue.isEmpty()){
+            if(bandera == true){
+                break;
+            }
+            Point p = queue.remove();
+            for(int i = 0; i < 4; i++){
+                int a = p.x + x[i];
+                int b = p.y + y[i];
+                if(a >= 0 && b >= 0 && a < n && b < m && visto[a][b] == false && map[a][b] != '+' && map[a][b] != '-' && map[a][b] != '|' ){
+                    visto[a][b] = true;
+                    resultado[a][b]='#';
+                    recorrido.add("(" + a + ","+b + ")");
+                    System.out.println();
+                    if(map[a][b]==map[endy][endx]){
+                        bandera = true;
+                        break;
+                    }
+                    queue.add(new Point(a,b));
+                }
+            }
+        }
+        for(String recorrer: recorrido){
+            System.out.println(recorrer);
+        }
+        return resultado;
+    }
+
+    public static void escribeArchivo(ArrayList<String> nodos, char[][] laberinto){
+
+        int i=0;
+        Queue<String> queue = new LinkedList<>();
+        for (String x: nodos) {
+            //System.out.println(x.nombre + "->" + y.nombre_destino + "[label=" + y.peso + "]");
+            if(i<nodos.size()-1) {
+                queue.add(nodos.get(i) + "->" + nodos.get(i+1));
+            }
+            i++;
+        }
+        try {
+            FileWriter fw = new FileWriter("laberinto.dot");
+            for (String oracion : queue) {
+                fw.write(oracion);
+                fw.write("\n");
+            }
+            for(char[] fila: laberinto){
+                for(char elemento: fila){
+                    fw.write(elemento);
+                    //System.out.print(elemento);
+                }
+                fw.write("\n");
+            }
+            fw.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Para leer el laberinto
